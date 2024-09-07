7b864de1b2499cd7a951980cf380e51842b7396c
zang <bt.hoanggiang@gmail.com>
2024-09-08 01:42:50 +0700

    fix: ssl

--- webserver/init-letsencrypt.sh
+++ webserver/init-letsencrypt.sh
@@ -6,10 +6,10 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(aqa-server)
+domains=(bthzang.id.vn)
 rsa_key_size=4096
 data_path="./webserver/certbot"
-email="otribe@geekup.io" # Adding a valid address is strongly recommended
+email="otribe@id.vn" # Adding a valid address is strongly recommended
 staging=0                # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
