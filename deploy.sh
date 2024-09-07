043a7c5e8ff6b28cafd9ca36780527b072d4dbde
zang <bt.hoanggiang@gmail.com>
2024-09-08 00:28:34 +0700

    feat: deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+web_server_name=webserver # service_name of webserver
+service_name=backend      # service_name deploy
+
+reload_nginx() {
+  nginx_id=$(docker ps -f name=$web_server_name -q | tail -n1)
+  docker exec $nginx_id nginx -s reload
+}
+
+deploy() {
+
+  if [ -z "$1" ]; then
+    # Get version
+    read -r -p "Enter version you want to deploy: " version
+  else
+    version=$1
+  fi
+
+  # Check if the input is empty
+  if [ -z "$version" ]; then
+    echo "You did not enter a version."
+    exit 1
+  fi
+
+  export VERSION=$version
+
+  echo "Start deploy backend...."
+  old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
+
+  # bring a new container online, running new code
+  # (nginx continues routing to the old container only)
+  echo "Create new container"
+ 
+  docker compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name 
+ 
+  echo -e "\nStart routing requests to the new container (as well as the old)"
+  reload_nginx
+
+  echo "Removing old contaner"
+  docker stop $old_container_id
+  docker rm $old_container_id
+  echo "Removed old container"
+
+  echo "Change scale set to 1"
+  docker compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
+  
+  echo "Stop routing requests to the old container"
+  reload_nginx
+  echo "Deploy backend successfully!!!"
+
+  echo "Clean up image"
+  docker image prune -a -f 
+}
+
+deploy $1
